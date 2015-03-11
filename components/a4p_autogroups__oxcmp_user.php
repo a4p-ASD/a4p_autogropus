987b9ed97f341b6431a5f4cc7121d4ef9ea313df
a4p ASD <asd@apps4print.com>
2015-03-11 15:10:59 +0100

    Dateien umbenannt; oxcmp_user::createUser von OXID CE 4.9.2

--- components/a4p_autogroups__oxcmp_user.php
+++ components/a4p_autogroups__oxcmp_user.php
@@ -6,12 +6,12 @@
  *
  *
  *	@version:	1.0.0
- *	@date:		09.04.2014
+ *	@date:		11.03.2015
  *
  *
- *	a4p_autogroups_oxcmp_user.php
+ *	a4p_autogroups__oxcmp_user.php
  *
- *	apps4print - a4p_autogroups - add new users to defined groups
+ *	apps4print - a4p_autogroups - neue Benutzer zu festgelegten Gruppen hinzufügen
  *
  */
 
@@ -19,42 +19,50 @@
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
 
-class a4p_autogroups_oxcmp_user extends a4p_autogroups_oxcmp_user_parent {
-	
+class a4p_autogroups__oxcmp_user extends a4p_autogroups__oxcmp_user_parent {
+
 	// ------------------------------------------------------------------------------------------------
 	// ------------------------------------------------------------------------------------------------
-	
-	#protected $o_a4p_debug_log					= null;
-	
+
+
+	protected $o_a4p_debug_log					= null;
+
+
 	// ------------------------------------------------------------------------------------------------
 	// ------------------------------------------------------------------------------------------------
-	
+
 	public function __construct() {
-		
-		parent::__construct();
-		
-		
-		#$this->o_a4p_debug_log					= oxNew( "a4p_debug_log" );
-		#$this->o_a4p_debug_log->a4p_debug_log_init( true, __CLASS__ . ".txt", null );
-		#$this->o_a4p_debug_log->a4p_debug_log_init( true, "a4p-log.txt", null );
-		
+
+
+		// ------------------------------------------------------------------------------------------------
+		// init a4p_debug_log
+		//
+		/*
+		$o_oxModule							= oxNew( "oxModule" );
+		$o_oxModule->load( "a4p_debug_log" );
+		if ( $o_oxModule->isActive() ) {
+
+			$this->o_a4p_debug_log			= oxNew( "a4p_debug_log" );
+			$this->o_a4p_debug_log->a4p_debug_log_init( true, __CLASS__ . ".txt", null );
+		}
+		//*/
+		// ------------------------------------------------------------------------------------------------
+
+
 	}
-	
+
 	// ------------------------------------------------------------------------------------------------
 
 	/**
-	 * @version		OXID CE 4.7.2
-	 * 
-	 * @warning		changes in OXID CE 4.8.6 or earlier
+	 * @version		OXID CE 4.9.2
 	 */
 
 	/**
 	 * First test if all MUST FILL fields were filled, then performed
 	 * additional checking oxcmp_user::CheckValues(). If no errors
 	 * occured - trying to create new user (oxuser::CreateUser()),
-	 * logging him to shop (oxuser::Login() if user has entered password)
-	 * or assigning him to dynamic group (oxuser::addDynGroup()).
-	 * If oxuser::CreateUser() returns false - thsi means user is
+	 * logging him to shop (oxuser::Login() if user has entered password).
+	 * If oxuser::CreateUser() returns false - this means user is
 	 * allready created - we only logging him to shop (oxcmp_user::Login()).
 	 * If there is any error with missing data - function will return
 	 * false and set error code (oxcmp_user::iError). If user was
@@ -70,180 +78,190 @@ class a4p_autogroups_oxcmp_user extends a4p_autogroups_oxcmp_user_parent {
 	 * @return  mixed	redirection string or true if successful, false otherwise
 	 */
 	public function createUser() {
-	
-		
+
+
 		$blActiveLogin = $this->getParent()->isEnabledPrivateSales();
-	
-		$myConfig = $this->getConfig();
-		if ( $blActiveLogin && !oxConfig::getParameter( 'ord_agb' ) && $myConfig->getConfigParam( 'blConfirmAGB' ) ) {
-			oxRegistry::get("oxUtilsView")->addErrorToDisplay( 'ORDER_READANDCONFIRMTERMS', false, true );
+
+		$oConfig = $this->getConfig();
+
+		if ($blActiveLogin && !$oConfig->getRequestParameter('ord_agb') && $oConfig->getConfigParam('blConfirmAGB')) {
+			oxRegistry::get("oxUtilsView")->addErrorToDisplay('READ_AND_CONFIRM_TERMS', false, true);
+
 			return;
 		}
-	
-		$myUtils  = oxRegistry::getUtils();
-	
+
 		// collecting values to check
-		$sUser = oxConfig::getParameter( 'lgn_usr' );
-	
+		$sUser = $oConfig->getRequestParameter('lgn_usr');
+
 		// first pass
-		$sPassword = oxConfig::getParameter( 'lgn_pwd', true );
-	
+		$sPassword = $oConfig->getRequestParameter('lgn_pwd', true);
+
 		// second pass
-		$sPassword2 = oxConfig::getParameter( 'lgn_pwd2', true );
-	
-		$aInvAdress = oxConfig::getParameter( 'invadr', true );
+		$sPassword2 = $oConfig->getRequestParameter('lgn_pwd2', true);
+
+		$aInvAdress = $oConfig->getRequestParameter('invadr', true);
 		$aDelAdress = $this->_getDelAddressData();
-	
-		$oUser = oxNew( 'oxuser' );
-	
+
+		/** @var oxUser $oUser */
+		$oUser = oxNew('oxuser');
+
 		try {
-	
-			$oUser->checkValues( $sUser, $sPassword, $sPassword2, $aInvAdress, $aDelAdress );
-	
+
+			$oUser->checkValues($sUser, $sPassword, $sPassword2, $aInvAdress, $aDelAdress);
+
 			$iActState = $blActiveLogin ? 0 : 1;
-	
+
 			// setting values
 			$oUser->oxuser__oxusername = new oxField($sUser, oxField::T_RAW);
-			$oUser->setPassword( $sPassword );
-			$oUser->oxuser__oxactive   = new oxField( $iActState, oxField::T_RAW);
-	
+			$oUser->setPassword($sPassword);
+			$oUser->oxuser__oxactive = new oxField($iActState, oxField::T_RAW);
+
 			// used for checking if user email currently subscribed
 			$iSubscriptionStatus = $oUser->getNewsSubscription()->getOptInStatus();
-	
+
 			$oUser->createUser();
-			$oUser->load( $oUser->getId() );
-			$oUser->changeUserData( $oUser->oxuser__oxusername->value, $sPassword, $sPassword, $aInvAdress, $aDelAdress );
-	
-			if ( $blActiveLogin ) {
+			$oUser->load($oUser->getId());
+			$oUser->changeUserData($oUser->oxuser__oxusername->value, $sPassword, $sPassword, $aInvAdress, $aDelAdress);
+
+			if ($blActiveLogin) {
 				// accepting terms..
 				$oUser->acceptTerms();
 			}
-	
-			$sUserId = oxSession::getVar( "su" );
-			$sRecEmail = oxSession::getVar( "re" );
-			if ( $this->getConfig()->getConfigParam( 'blInvitationsEnabled' ) && $sUserId && $sRecEmail ) {
+
+			$sUserId = oxRegistry::getSession()->getVariable("su");
+			$sRecEmail = oxRegistry::getSession()->getVariable("re");
+			if ($this->getConfig()->getConfigParam('blInvitationsEnabled') && $sUserId && $sRecEmail) {
 				// setting registration credit points..
-				$oUser->setCreditPointsForRegistrant( $sUserId, $sRecEmail );
+				$oUser->setCreditPointsForRegistrant($sUserId, $sRecEmail);
 			}
-	
+
 			// assigning to newsletter
-			$blOptin = oxRegistry::getConfig()->getRequestParameter( 'blnewssubscribed' );
-			if ( $blOptin && $iSubscriptionStatus == 1 ) {
-				// if user was assigned to newsletter and is creating account with newsletter checked, don't require confirm
+			$blOptin = oxRegistry::getConfig()->getRequestParameter('blnewssubscribed');
+			if ($blOptin && $iSubscriptionStatus == 1) {
+				// if user was assigned to newsletter
+				// and is creating account with newsletter checked,
+				// don't require confirm
 				$oUser->getNewsSubscription()->setOptInStatus(1);
-				$oUser->addToGroup( 'oxidnewsletter' );
+				$oUser->addToGroup('oxidnewsletter');
 				$this->_blNewsSubscriptionStatus = 1;
 			} else {
-				$this->_blNewsSubscriptionStatus = $oUser->setNewsSubscription( $blOptin, $this->getConfig()->getConfigParam( 'blOrderOptInEmail' ) );
+				$blOrderOptInEmailParam = $this->getConfig()->getConfigParam('blOrderOptInEmail');
+				$this->_blNewsSubscriptionStatus = $oUser->setNewsSubscription($blOptin, $blOrderOptInEmailParam);
 			}
-	
-			$oUser->addToGroup( 'oxidnotyetordered' );
-			$oUser->addDynGroup( oxSession::getVar( 'dgr' ), $myConfig->getConfigParam( 'aDeniedDynGroups' ) );
-			
-			
-			// ------------------------------------------------------------------------------------------------
-			// a4p ASD - erweiterte, automatische Gruppenzuordnung
-			
-			$this->a4p_add_to_auto_group( $oUser );
-			// ------------------------------------------------------------------------------------------------
-			
-			
+
+			$oUser->addToGroup('oxidnotyetordered');
+
+
+// ------------------------------------------------------------------------------------------------
+// a4p ASD - erweiterte, automatische Gruppenzuordnung
+
+$this->a4p_add_to_auto_group( $oUser );
+// ------------------------------------------------------------------------------------------------
+
+
 			$oUser->logout();
-	
-		} catch ( oxUserException $oEx ) {
-			oxRegistry::get("oxUtilsView")->addErrorToDisplay( $oEx, false, true );
+
+		} catch (oxUserException $oEx) {
+			oxRegistry::get("oxUtilsView")->addErrorToDisplay($oEx, false, true);
+
 			return false;
-		} catch( oxInputException $oEx ){
-			oxRegistry::get("oxUtilsView")->addErrorToDisplay( $oEx, false, true );
+		} catch (oxInputException $oEx) {
+			oxRegistry::get("oxUtilsView")->addErrorToDisplay($oEx, false, true);
+
 			return false;
-		} catch( oxConnectionException $oEx ){
-			oxRegistry::get("oxUtilsView")->addErrorToDisplay( $oEx, false, true );
+		} catch (oxConnectionException $oEx) {
+			oxRegistry::get("oxUtilsView")->addErrorToDisplay($oEx, false, true);
+
 			return false;
 		}
-	
-		if ( !$blActiveLogin ) {
-			if ( !$sPassword ) {
-				oxSession::setVar( 'usr', $oUser->getId() );
-				$this->_afterLogin( $oUser );
-			} elseif ( $this->login() == 'user' ) {
-				return false;
-			}
-	
+
+		if (!$blActiveLogin) {
+
+			oxRegistry::getSession()->setVariable('usr', $oUser->getId());
+			$this->_afterLogin($oUser);
+
+
 			// order remark
 			//V #427: order remark for new users
-			$sOrderRemark = oxConfig::getParameter( 'order_remark', true );
-			if ( $sOrderRemark ) {
-				oxSession::setVar( 'ordrem', $sOrderRemark );
+			$sOrderRemark = oxRegistry::getConfig()->getRequestParameter('order_remark', true);
+			if ($sOrderRemark) {
+				oxRegistry::getSession()->setVariable('ordrem', $sOrderRemark);
 			}
 		}
-	
+
 		// send register eMail
 		//TODO: move into user
-		if ( (int) oxConfig::getParameter( 'option' ) == 3 ) {
-			$oxEMail = oxNew( 'oxemail' );
-			if ( $blActiveLogin ) {
-				$oxEMail->sendRegisterConfirmEmail( $oUser );
+		if ((int) oxRegistry::getConfig()->getRequestParameter('option') == 3) {
+			$oxEMail = oxNew('oxemail');
+			if ($blActiveLogin) {
+				$oxEMail->sendRegisterConfirmEmail($oUser);
 			} else {
-				$oxEMail->sendRegisterEmail( $oUser );
+				$oxEMail->sendRegisterEmail($oUser);
 			}
 		}
-	
+
 		// new registered
 		$this->_blIsNewUser = true;
-	
-		return 'payment';
+
+		$sAction = 'payment?new_user=1&success=1';
+		if ($this->_blNewsSubscriptionStatus !== null && !$this->_blNewsSubscriptionStatus) {
+			$sAction = 'payment?new_user=1&success=1&newslettererror=4';
+		}
+
+		return $sAction;
+
+
 	}
-	
+
 	// ------------------------------------------------------------------------------------------------
-	
+
 	protected function a4p_add_to_auto_group( $oUser ) {
-	
-		
+
+
 		// ------------------------------------------------------------------------------------------------
 		// Benutzer bei Eingabe von Firma + UST-ID automatisch in Gruppe "Händler" ( "oxiddealer" ) aufnehmen
 
 		$bSetting_addToDealer					= $this->getConfig()->getConfigParam( "a4p_autogroups_addToDealer" );
-		
+
 		if ( $bSetting_addToDealer ) {
-				
+
 			$bCheckDealerFields					= true;
-		
+
 			// ------------------------------------------------------------------------------------------------
 			// relevante Felder prüfen
 			if ( !$oUser->oxuser__oxcompany->value )
 				$bCheckDealerFields				= false;
-		
+
 			if ( !$oUser->oxuser__oxustid->value )
 				$bCheckDealerFields				= false;
-		
+
 			//checkVatId( $oUser, $aInvAddress )
-		
+
 			// ------------------------------------------------------------------------------------------------
 			// UST-ID genauer prüfen
 			if ( !$this->a4p_checkUstId( $oUser ) ) {
 				$bCheckDealerFields				= false;
 			}
 			// ------------------------------------------------------------------------------------------------
-			
-			
+
+
 			// ------------------------------------------------------------------------------------------------
 			// bei erfolgreicher Prüfung in Gruppe aufnehmen
 			#if ( $bCheckDealerFields && !$this->getUser()->inGroup( "oxiddealer" ) ) {
 			if ( $bCheckDealerFields && !$oUser->inGroup( "oxiddealer" ) ) {
 
-				#$this->getUser()->addToGroup( "oxiddealer" );		
-				$oUser->addToGroup( "oxiddealer" );		
-			}		
+				$oUser->addToGroup( "oxiddealer" );
+			}
 		}
 		// ------------------------------------------------------------------------------------------------
-		
-		
+
+
 		// ------------------------------------------------------------------------------------------------
 		// Module-Setting für neue Gruppenaufnahme holen
 
 		$sSetting_addToGroup					= $this->getConfig()->getConfigParam( "a4p_autogroups_addToGroup" );
 		$aSetting_addToGroup					= explode( ",", $sSetting_addToGroup );
-		
+
 		foreach( $aSetting_addToGroup as $key => $curGroup ) {
 
 			$curGroup							= trim( $curGroup );
@@ -252,17 +270,17 @@ class a4p_autogroups_oxcmp_user extends a4p_autogroups_oxcmp_user_parent {
 			$oUser->addToGroup( $curGroup );
 		}
 		// ------------------------------------------------------------------------------------------------
-		
-		
+
+
 		// ------------------------------------------------------------------------------------------------
 		// Benutzer in Gruppe mit gewähltem Land aufnehmen
 
 		$bSetting_addToCountryGroup				= $this->getConfig()->getConfigParam( "a4p_autogroups_addToCountryGroup" );
 
 		if ( $bSetting_addToCountryGroup ) {
-				
+
 			$sCurUserCountryID					= $oUser->oxuser__oxcountryid->value;
-				
+
 			// ------------------------------------------------------------------------------------------------
 			// Land für oxcountryid und Gruppe mit selben Title ermitteln
 			$sSQL								= "SELECT oxgroups.OXID FROM oxgroups";
@@ -275,34 +293,34 @@ class a4p_autogroups_oxcmp_user extends a4p_autogroups_oxcmp_user_parent {
 				$oxgroups__oxid					= $rows->fields[ "OXID" ];
 			}
 			if ( $oxgroups__oxid ) {
-					
+
 				#$this->getUser()->addToGroup( $oxgroups__oxid );
 				$oUser->addToGroup( $oxgroups__oxid );
 			}
-			
+
 		}
 		// ------------------------------------------------------------------------------------------------
-		
+
 	}
 
 	// ------------------------------------------------------------------------------------------------
-	
+
 	/**
 	 * @desc	UST-ID genauer prüfen
 	 */
-	
+
 	protected function a4p_checkUstId( $oUser ) {
 
-		
+
 		// ------------------------------------------------------------------------------------------------
 		// Land laden
 		$sCountryId								= $oUser->oxuser__oxcountryid->value;
 		if ( !$sCountryId ) {
-	
+
 			return false;
-	
+
 		} else {
-	
+
 			$oCountry							= oxNew( 'oxcountry' );
 			$oCountry->load( $sCountryId );
 
@@ -330,11 +348,11 @@ class a4p_autogroups_oxcmp_user extends a4p_autogroups_oxcmp_user_parent {
 			else
 				return false;
 		}
-	
+
 	}
-	
+
 	// ------------------------------------------------------------------------------------------------
-	
+
 }
 
 // ------------------------------------------------------------------------------------------------
--- metadata.php
+++ metadata.php
@@ -5,13 +5,13 @@
  *	@company:	apps4print / page one GmbH, Nürnberg, Germany
  *
  *
- *	@version:	1.0.0
- *	@date:		13.06.2014
+ *	@version:	1.0.1
+ *	@date:		11.03.2015
  *
  *
  *	metadata.php
  *
- *	apps4print - a4p_autogroups - add new users to defined groups
+ *	apps4print - a4p_autogroups - neue Benutzer zu festgelegten Gruppen hinzufügen
  *
  */
 
@@ -23,19 +23,19 @@ $sMetadataVersion = '1.0';
 
 $aModule = array(
 	'id'			=> 'a4p_autogroups',
-	'title'			=> 'apps4print - a4p_autogroups - OXID 4.7.2',
+	'title'			=> 'apps4print - a4p_autogroups',
 	'description'	=> array(
 		'de'									=> 'neue Benutzer zu festgelegten Gruppen hinzufügen', 
 		'en'									=> 'add new users to defined groups'
 	),
 	'lang'			=> 'de',
 	'thumbnail'		=> 'out/img/apps4print/a4p_logo.jpg',
-	'version'		=> '<a4p_VERSION> (1.0.0)',
+	'version'		=> '<a4p_VERSION> (1.0.1)',
 	'author'		=> 'apps4print',
 	'url'			=> 'http://www.apps4print.com',
 	'email'			=> 'support@apps4print.com',
 	'extend'	  	=> array(
-		'oxcmp_user'							=> 'apps4print/a4p_autogroups/components/a4p_autogroups_oxcmp_user'
+		'oxcmp_user'							=> 'apps4print/a4p_autogroups/components/a4p_autogroups__oxcmp_user'
 	),
 	'files'			=> array(
 	),
